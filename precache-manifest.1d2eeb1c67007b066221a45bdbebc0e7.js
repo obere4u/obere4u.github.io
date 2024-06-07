a542656ceea8565a3ec294a3fc952836efffe0d8
Nwosa <obere4u@gmail.com>
2024-06-07 16:13:06 +0100

    Updates

---
+++ precache-manifest.1d2eeb1c67007b066221a45bdbebc0e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942e915f533acbf0750082ab87e8ceb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a2282c2be5b44defbc",
+    "url": "/static/css/main.685ea860.chunk.css"
+  },
+  {
+    "revision": "92bc8f032efbda47b5ea",
+    "url": "/static/js/2.496e6672.chunk.js"
+  },
+  {
+    "revision": "12a2282c2be5b44defbc",
+    "url": "/static/js/main.9bcbc0c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
